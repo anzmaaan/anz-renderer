d7818681d9329a2dd226ca074fbe7b8764e8f98c
Anselm Wagner <wagner.anselm@gmail.com>
2022-04-16 23:38:12 +0200

    initial commit

---
+++ HelloPygame.py
@@ -0,0 +1,10 @@
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+screen_width = 1000
+screen_height = 800
+
+screen = pygame.display.set_mode((screen_width, screen_height), DOUBLEBUF | OPENGL)
+pygame.display.set_caption('OpneGL in Python')
