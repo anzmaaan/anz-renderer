ed7ac2b022a42f95c553593f864ed1bb9ca97d63
Anselm Wagner <wagner.anselm@gmail.com>
2022-04-17 01:18:51 +0200

    Initial comnmit. Note: line 18, changed orientation of y to start from upper left corner (like pygame).

---
+++ MousePlot.py
@@ -0,0 +1,46 @@
+import pygame
+from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+pygame.init()
+
+screen_width = 1000
+screen_height = 800
+
+screen = pygame.display.set_mode((screen_width, screen_height), DOUBLEBUF | OPENGL)
+pygame.display.set_caption('Graphs in PyOpenGL')
+
+
+def init_ortho():
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluOrtho2D(0, 1000, 800, 0)
+
+
+def plot_point():
+    glBegin(GL_POINTS)
+    for p in points:
+        glVertex2f(p[0], p[1])
+    glEnd()
+
+
+done = False
+init_ortho()
+glPointSize(5)
+points = []
+while not done:
+    p = None
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        elif event.type == MOUSEBUTTONDOWN:
+            points.append(pygame.mouse.get_pos())
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+    plot_point()
+    pygame.display.flip()
+    pygame.time.wait(100)
+pygame.quit()
