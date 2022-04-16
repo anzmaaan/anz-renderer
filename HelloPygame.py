39f64fdc4bc3b7d07757a31a792ba26edeb3a2cc
Anselm Wagner <wagner.anselm@gmail.com>
2022-04-16 23:55:18 +0200

    added 'game loop' and quit functionality

--- HelloPygame.py
+++ HelloPygame.py
@@ -9,3 +9,12 @@ screen_height = 800
 screen = pygame.display.set_mode((screen_width, screen_height), DOUBLEBUF | OPENGL)
 pygame.display.set_caption('OpenGL in Python')
 
+done = False
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+    pygame.display.flip()
+    pygame.time.wait(100)
+
+pygame.quit()
