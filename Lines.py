c682c73385cb5cdfe1d45a721fc91e8eff3146ca
Anselm Wagner <github@anselmwagner.de>
2022-04-23 18:58:09 +0200

    Initial commit.

---
+++ Lines.py
@@ -0,0 +1,74 @@
+import pygame
+from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from Utils import *
+
+pygame.init()
+
+screen_width = 1000
+screen_height = 800
+ortho_width = 640
+ortho_height = 480
+
+screen = pygame.display.set_mode((screen_width, screen_height), DOUBLEBUF | OPENGL)
+pygame.display.set_caption('Lines in PyOpenGL')
+
+
+def init_ortho():
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluOrtho2D(0, ortho_width, 0, ortho_height)
+
+
+def plot_point():
+    glBegin(GL_POINTS)
+    for p in points:
+        glVertex2f(p[0], p[1])
+    glEnd()
+
+
+def plot_lines():
+    for l in points:
+        glBegin(GL_LINE_STRIP)
+        for coords in l:
+            glVertex2f(coords[0], coords[1])
+        glEnd()
+
+
+done = False
+init_ortho()
+glPointSize(5)
+points = []
+line = []
+mouse_down = False
+
+while not done:
+    p = None
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        elif event.type == MOUSEBUTTONDOWN:
+            mouse_down = True
+            line = []
+            points.append(line)
+        elif event.type == MOUSEBUTTONUP:
+            mouse_down = False
+        elif event.type == MOUSEMOTION and mouse_down is True:
+            p = pygame.mouse.get_pos()
+            '''orto_height steht vorne, weil die 
+            koordinatensysteme von pygame (0,0 = top left) und opengl (0,0 = bottom left) 
+            unterschiedliche ursprünge haben.'''
+            line.append((map_value(0, screen_width, 0, ortho_width, p[0]),
+                           map_value(0, screen_height, ortho_height, 0, p[1])))
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+    plot_lines()
+    pygame.display.flip()
+    pygame.time.wait(500)
+    print(f'LINE: {line}')
+    print(f'POINTS: {points}')
+
+pygame.quit()
