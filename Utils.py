dcc81ce0978b297691fba27f361f9ec49f0c1e0f
Anselm Wagner <github@anselmwagner.de>
2022-04-23 17:40:17 +0200

    Added comment for map_value()

--- Utils.py
+++ Utils.py
@@ -1,3 +1,4 @@
+# remaps the value of cursor position from pygame to opengl resolutions
 def map_value(current_min, current_max, new_min, new_max, value):
     current_range = current_max - current_min
     new_range = new_max - new_min
