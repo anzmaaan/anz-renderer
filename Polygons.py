b77418c4d9fb412d9d0526063ad4b396fad75e62
Anselm Wagner <github@anselmwagner.de>
2022-04-24 23:23:58 +0200

    Initial commit.

---
+++ Polygons.py
@@ -0,0 +1,58 @@
+import pygame
+from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from Utils import *
+
+pygame.init()
+
+screen_width = 800
+screen_height = 800
+ortho_left = -400
+ortho_right = 400
+ortho_top = -400
+ortho_bottom = 400
+
+screen = pygame.display.set_mode((screen_width, screen_height), DOUBLEBUF | OPENGL)
+pygame.display.set_caption('Polygons in PyOpenGL')
+
+
+def init_ortho():
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluOrtho2D(ortho_left, ortho_right, ortho_top, ortho_bottom)
+
+
+def plot_polygon():
+    glColor(1, 1, 1)
+    glBegin(GL_POLYGON)
+    for p in points:
+        glVertex2f(p[0], p[1])
+    glEnd()
+    glColor(1, 0, 0)
+    glLineWidth(5)
+    glBegin(GL_LINE_LOOP)
+    for p in points:
+        glVertex2f(p[0], p[1])
+    glEnd()
+
+
+done = False
+init_ortho()
+points = []
+while not done:
+    p = None
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        elif event.type == MOUSEBUTTONDOWN:
+            p = pygame.mouse.get_pos()
+            points.append((map_value(0, screen_width, ortho_left, ortho_right, p[0]),
+                           map_value(0, screen_height, ortho_bottom, ortho_top, p[1])))
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+    plot_polygon()
+    pygame.display.flip()
+pygame.quit()
