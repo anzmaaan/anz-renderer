3a53a2d8bb507f6cd68de2c39bd7b0b5c98a5ff5
Anselm Wagner <github@anselmwagner.de>
2022-04-24 21:22:04 +0200

    Changed file to plot a graph. Imported new modules, introduce new variables, reused plot_graph() from PlotPoints.py and changed it to work with lines, removed print statements for debugging (my brain).

--- Lines.py
+++ Lines.py
@@ -4,10 +4,17 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 from Utils import *
 
+import math
+import numpy as np
+
 pygame.init()
 
 screen_width = 1000
 screen_height = 800
+ortho_top = 1
+ortho_bottom = -1
+ortho_left = 0
+ortho_right = 4
 ortho_width = 640
 ortho_height = 480
 
@@ -18,13 +25,23 @@ pygame.display.set_caption('Lines in PyOpenGL')
 def init_ortho():
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
-    gluOrtho2D(0, ortho_width, 0, ortho_height)
+    gluOrtho2D(ortho_left, ortho_right, ortho_bottom, ortho_top)
 
 
 def plot_point():
     glBegin(GL_POINTS)
     for p in points:
         glVertex2f(p[0], p[1])
+    glEnd()
+
+
+def plot_graph():
+    glBegin(GL_LINE_STRIP)
+    px: GL_DOUBLE
+    py: GL_DOUBLE
+    for px in np.arange(0, 4, 0.005):
+        py = math.exp(-px) * math.cos(2 * math.pi * px)
+        glVertex2f(px, py)
     glEnd()
 
 
@@ -59,16 +76,14 @@ while not done:
             '''orto_height steht vorne, weil die 
             koordinatensysteme von pygame (0,0 = top left) und opengl (0,0 = bottom left) 
             unterschiedliche ursprünge haben.'''
-            line.append((map_value(0, screen_width, 0, ortho_width, p[0]),
-                           map_value(0, screen_height, ortho_height, 0, p[1])))
+            line.append((map_value(0, screen_width, ortho_left, ortho_right, p[0]),
+                           map_value(0, screen_height, ortho_bottom, ortho_top, p[1])))
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glMatrixMode(GL_MODELVIEW)
     glLoadIdentity()
-    plot_lines()
+    plot_graph()
     pygame.display.flip()
     pygame.time.wait(500)
-    print(f'LINE: {line}')
-    print(f'POINTS: {points}')
 
 pygame.quit()
