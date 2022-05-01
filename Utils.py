a161efdd54f8f1e8e278e51090229b6b23d7e9ba
Anselm Wagner <github@anselmwagner.de>
2022-05-01 18:50:41 +0200

    Update from h3d pyopengl 0401.

--- Utils.py
+++ Utils.py
@@ -1,5 +1,23 @@
+import math
+import numpy as np
+
 # remaps the value of cursor position from pygame to opengl resolutions
 def map_value(current_min, current_max, new_min, new_max, value):
     current_range = current_max - current_min
     new_range = new_max - new_min
     return new_min + new_range * ((value - current_min)/current_range)
+
+
+def x_rotation(vector, theta):
+    new_vector = np.array([[1, 0, 0], [0, np.cos(theta), -np.sin(theta)], [0, np.sin(theta), np.cos(theta)]])
+    return np.dot(new_vector, vector)
+
+
+def y_rotation(vector, theta):
+    new_vector = np.array([[np.cos(theta), 0, np.sin(theta)], [0, 1, 0], [-np.sin(theta), 0, np.cos(theta)]])
+    return np.dot(new_vector, vector)
+
+
+def z_rotation(vector, theta):
+    new_vector = np.array([[np.cos(theta), -np.sin(theta), 0], [np.sin(theta), np.cos(theta), 0], [0, 0, 1]])
+    return np.dot(new_vector, vector)
