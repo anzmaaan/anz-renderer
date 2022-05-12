b0b7d6017180c699c2e0d63521ef9636e0588dcc
Anselm Wagner <github@anselmwagner.de>
2022-05-12 16:57:27 +0200

    Initial Commit.

---
+++ Engine/Mesh.py
@@ -0,0 +1,21 @@
+from OpenGL.GL import *
+import pygame
+
+class Mesh:
+    def __init__(self):
+        self.vertices = [(0.5, -0.5, 0.5),
+            (-0.5, -0.5, 0.5),
+            (0.5, 0.5, 0.5),
+            (-0.5, 0.5, 0.5),
+            (0.5, 0.5, -0.5),
+            (-0.5, 0.5, -0.5)]
+        self.triangles = [0, 2, 3, 0, 3, 1]
+        self.draw_type = GL_LINE_LOOP
+
+    def draw(self):
+        for t in range(0, len(self.triangles), 3):
+            glBegin(self.draw_type)
+            glVertex3fv(self.vertices[self.triangles[t]])
+            glVertex3fv(self.vertices[self.triangles[t + 1]])
+            glVertex3fv(self.vertices[self.triangles[t + 2]])
+            glEnd()
