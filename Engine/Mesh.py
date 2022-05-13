ce0cc5616d585589ebfcb6f93e468810b2c61619
Anselm Wagner <github@anselmwagner.de>
2022-05-13 17:36:49 +0200

    Added parameters.

--- Engine/Mesh.py
+++ Engine/Mesh.py
@@ -13,8 +13,10 @@ class Mesh:
         self.triangles = [0, 2, 3, 0, 3, 1]
         self.draw_type = GL_LINE_LOOP
 
-    def draw(self):
+    def draw(self, line_width=1, viewport_color=(1, 1, 1)):
         for t in range(0, len(self.triangles), 3):
+            glLineWidth(line_width)
+            glColor(viewport_color)
             glBegin(self.draw_type)
             glVertex3fv(self.vertices[self.triangles[t]])
             glVertex3fv(self.vertices[self.triangles[t + 1]])
