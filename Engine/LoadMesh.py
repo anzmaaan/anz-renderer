54b425f0319d62fd5b39c0cb7b1093a362ddc65f
Anselm Wagner <github@anselmwagner.de>
2022-05-27 18:18:46 +0200

    Added parameter 'position'. Removed 'self' from 'vertices' and 'triangles', no longer needed for own object, but used now as parameters for calling parent class with 'super()'.

--- Engine/LoadMesh.py
+++ Engine/LoadMesh.py
@@ -4,23 +4,24 @@ import pygame
 
 
 class LoadMesh(Mesh):
-    def __init__(self, filename, draw_type):
-        self.vertices = []
-        self.triangles = []
+    def __init__(self, filename, draw_type, position=pygame.Vector3(0, 0, 0)):
         self.filename = filename
-        self.draw_type = draw_type
-        self.load_drawing()
+        vertices, triangles = self.load_drawing()
+        super().__init__(vertices, triangles, draw_type, position)
 
     def load_drawing(self):
+        vertices = []
+        triangles = []
         with open(self.filename) as fp:
             line = fp.readline()
             while line:
                 if line[:2] == "v ":
                     vx, vy, vz = [float(value) for value in line[2:].split()]
-                    self.vertices.append((vx, vy, vz))
+                    vertices.append((vx, vy, vz))
                 if line[:2] == "f ":
                     t1, t2, t3 = [value for value in line[2:].split()]
-                    self.triangles.append([int(value) for value in t1.split('/')][0]-1)
-                    self.triangles.append([int(value) for value in t2.split('/')][0]-1)
-                    self.triangles.append([int(value) for value in t3.split('/')][0]-1)
+                    triangles.append([int(value) for value in t1.split('/')][0] - 1)
+                    triangles.append([int(value) for value in t2.split('/')][0] - 1)
+                    triangles.append([int(value) for value in t3.split('/')][0] - 1)
                 line = fp.readline()
+        return vertices, triangles
