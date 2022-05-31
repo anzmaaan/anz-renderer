cce5d5e7c8fbbfc5abe21557dc9cb3a6736ef2ed
Anselm Wagner <github@anselmwagner.de>
2022-05-31 13:13:33 +0200

    Relocated and refactored.

--- Engine/Cube.py
+++ Engine/Cube.py
@@ -4,7 +4,8 @@ import pygame
 
 
 class Cube(Mesh):
-    def __init__(self, draw_type, position=pygame.Vector3(0, 0, 0)):
+    def __init__(self, draw_type, position=pygame.Vector3(0, 0, 0), rotation=Rotation(0, pygame.Vector3(0, 1, 0)),
+                 scale=pygame.Vector3(1, 1, 1)):
         vertices = [(0.5, -0.5, 0.5),
                     (-0.5, -0.5, 0.5),
                     (0.5, 0.5, 0.5),
@@ -31,4 +32,4 @@ class Cube(Mesh):
                     (0.5, -0.5, 0.5)]
         triangles = [0, 2, 3, 0, 3, 1, 8, 4, 5, 8, 5, 9, 10, 6, 7, 10, 7, 11, 12,
                      13, 14, 12, 14, 15, 16, 17, 18, 16, 18, 19, 20, 21, 22, 20, 22, 23]
-        super().__init__(vertices, triangles, draw_type, position)
+        super().__init__(vertices, triangles, draw_type, position, rotation, scale)
