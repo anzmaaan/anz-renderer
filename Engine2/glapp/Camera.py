791c25ffd439b07c8dd3349403f92d8bcdb8f961
Anselm Wagner <github@anselmwagner.de>
2022-06-19 00:06:48 +0200

    Changed code to work with matrices instead of OpenGL methods.

--- Engine2/glapp/Camera.py
+++ Engine2/glapp/Camera.py
@@ -1,35 +1,33 @@
+import numpy as np
 import pygame
 from OpenGL.GLU import *
 from math import *
+from .Transformations import *
 
 
 class Camera:
-    def __init__(self):
-        self.eye = pygame.math.Vector3(0, 0, 0)
-        self.up = pygame.math.Vector3(0, 1, 0)
-        self.right = pygame.math.Vector3(1, 0, 0)
-        self.forward = pygame.math.Vector3(0, 0, 1)
-        self.look = self.eye + self.forward
-        self.yaw = -90
-        self.pitch = 0
+    def __init__(self, program_id, w, h):
+        self.transformation = identity_mat()
         self.last_mouse = pygame.math.Vector2(0, 0)
         self.mouse_sensitivityX = 0.1
         self.mouse_sensitivityY = 0.1
         self.key_sensitivity = 0.008
+        self.projection_mat = self.perspective_mat(60, w/h, 0.01, 10000)
+
+    def perspective_mat(self, angle_of_view, aspect_ratio, near_plane, far_plane):
+        a = radians(angle_of_view)
+        d = 1.0/tan(a/2)
+        r = aspect_ratio
+        b = (far_plane + near_plane) / (near_plane - far_plane)
+        c = far_plane * near_plane / (near_plane - far_plane)
+        return np.array([[d/r, 0, 0, 0],
+                        [0, d, 0, 0],
+                        [0, 0, b, c],
+                        [0, 0, -1, 0]], np.float32)
 
     def rotate(self, yaw, pitch):
-        self.yaw += yaw
-        self.pitch += pitch
-        if self.pitch > 89.0:
-            self.pitch = 89.0
-        if self.pitch < -89.0:
-            self.pitch = -89.0
-        self.forward.x = cos(radians(self.yaw)) * cos(radians(self.pitch))
-        self.forward.y = sin(radians(self.pitch))
-        self.forward.z = sin(radians(self.yaw)) * cos(radians(self.pitch))
-        self.forward = self.forward.normalize()
-        self.right = self.forward.cross(pygame.Vector3(0, 1, 0)).normalize()
-        self.up = self.right.cross(self.forward).normalize()
+        self.transformation = rotate(self.transformation, yaw, "Y")
+        self.transformation = rotate(self.transformation, pitch, "X")
 
     def update(self, w, h):
         if pygame.mouse.get_visible():
@@ -38,17 +36,17 @@ class Camera:
         mouse_change = self.last_mouse - pygame.math.Vector2(mouse_pos)
         pygame.mouse.set_pos(w / 2, h / 2)
         self.last_mouse = pygame.mouse.get_pos()
-        self.rotate(-mouse_change.x * self.mouse_sensitivityX, mouse_change.y * self.mouse_sensitivityY)
+        self.rotate(mouse_change.x * self.mouse_sensitivityX, mouse_change.y * self.mouse_sensitivityY)
 
         keys = pygame.key.get_pressed()
         if keys[pygame.K_DOWN]:
-            self.eye -= self.forward * self.key_sensitivity
+            self.transformation = translate(self.transformation, 0, 0, self.key_sensitivity)
         if keys[pygame.K_UP]:
-            self.eye += self.forward * self.key_sensitivity
+            self.transformation = translate(self.transformation, 0, 0, -self.key_sensitivity)
         if keys[pygame.K_RIGHT]:
-            self.eye += self.right * self.key_sensitivity
+            self.transformation = translate(self.transformation, self.key_sensitivity, 0, 0)
         if keys[pygame.K_LEFT]:
-            self.eye -= self.right * self.key_sensitivity
+            self.transformation = translate(self.transformation, -self.key_sensitivity, 0, 0)
 
         self.look = self.eye + self.forward
         gluLookAt(self.eye.x, self.eye.y, self.eye.z,
